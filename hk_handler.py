3a5866d07daee93a13d126d7ed3d912668cfcc7f
Eric Valenzuela <evalenzuela@thousandeyes.com>
2020-02-07 10:36:01 -0800

    working http response w/headers

--- hk_handler.py
+++ hk_handler.py
@@ -1,24 +1,53 @@
 import socket
 import os
 import configparser
+import re
 
-class ThreadKill(Exception):
-    print("we're dunzo")
+def hk_handler(port_config, data, client_connection, args):
+    print('hk_handler')
+    port_type, resp_dict, resp_headers = get_port_config_settings(port_config, args)
+    # Check request data against responses
+    for pair in resp_dict:
+        if re.match(bytes(pair[0], encoding='utf-8'), data, re.IGNORECASE):
+            if args.v:
+                print(pair[0] + ' match found! Sending response.')
 
-def hk_handler(port_config, args):
-    cfp = configparser.ConfigParser()
+            # Construct full response
+            response_data = ''
+            for line in resp_headers:
+                response_data += (line + '\n')
+            response_data += '\n'
+            response_data += pair[1]
+            response_data += '\r\n'
+
+            # Send response to client
+            client_connection.sendall(bytes(response_data, encoding='utf-8'), 0)
+            client_connection.close()
+            return
+    client_connection.close()
+    return
+    
+def get_port_config_settings(port_config, args):
     try:
-        cfp.read(args.handler_dir+port_config)
+        cfp = configparser.ConfigParser()
+        path = args.handler_dir+str(port_config)
+        cfp.read(path)
     except configparser.ParsingError as cppe:
         print(cppe)
-        print('Error parsing '+str(port_config)+'file. Exiting.')
-        raise ThreadKill
-    port = cfp['port']
-    port_type = cfp['service_type']
+        print('Error parsing '+str(port_config)+' file. Exiting.')
+    except TypeError as te:
+        print(te)
+    port = cfp['main']['port']
+    port_type = cfp['main']['service_type']
     resp_dict = []
     for value in cfp['responses']:
         resp_dict.append([value, cfp['responses'][value]])
     if args.v:
         print(resp_dict)
+    resp_headers = []
+    for value in cfp['response_headers']:
+        resp_headers.append(cfp['response_headers'][value])
+    if args.v:
+        print(resp_headers)
+    return port_type, resp_dict, resp_headers
 
-    return
--- honeyknot.py
+++ honeyknot.py
@@ -3,6 +3,7 @@ import argparse
 import configparser
 import hk_handler
 import os, sys, time
+import socket
 
 """
 Honeyknot: The Highly-Configurable Honeypot
@@ -40,12 +41,26 @@ def server_port_process(port_config_file, args):
 def server_port_thread_pool(port_config, args):
     # Create thread pool
     with ThreadPoolExecutor(5) as spte:
+        # Create socket
+        print('standing-up socket on '+str(port_config)+'...')
+        try:
+            conn_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            conn_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            conn_sock.bind((args.bind_ip, 8080))
+        except Exception as se:
+            print('socket construction error: ')
+            print(se)
+            sys.exit(1)
         futures = []
         for i in range(5):
             if args.tv:
                 print('|- running port handler '+str(port_config)+', thread '+str(i))
-            futures.append(spte.submit(server_port_thread, [port_config, i], args))
+            futures.append(spte.submit(server_port_thread, [port_config, i], None, args))
         while True:
+            # Move socket to listening
+            conn_sock.listen(1)
+
+            # Grab idle thread
             for future in as_completed(futures):
                 if future.exception():
                     print(future.result())
@@ -55,19 +70,34 @@ def server_port_thread_pool(port_config, args):
                         print('|- spawning replacement port handler for '\
                                 'str(port_config)'\
                                 ', thread '+str(thread_num))
-                    futures.append(spte.submit(server_port_thread, [port_config, thread_num], args))
+                    
+                    # Feed incoming request to idle thread
+                    client_sock, client_address = conn_sock.accept()
+                    print('connection from '+str(client_address)+' on port '+str(port_config))
+
+                    futures.append(spte.submit(server_port_thread, [port_config, thread_num], client_sock, args))
     return futures
 
-def server_port_thread(counters, args):
+def server_port_thread(counters, client_connection, args):
     print('|-- port handler: '+str(counters[0])+', thread: '+str(counters[1]))
-    time.sleep(2)
+    
+    # If first run, return
+    if client_connection == None:
+        return counters[1]
+    else:
+        data = client_connection.recv(2048)
+        print(data)
     # Execute hk_handler
-    hk_handler(counters[0], args)
+    try:
+        hk_handler.hk_handler(counters[0], data, client_connection, args)
+    except Exception as tk:
+        print(tk)
     return counters[1]
 
 def run():
     # Argument Processing
     parser = argparse.ArgumentParser()
+    parser.add_argument('-ip', '--bind_ip', dest='bind_ip', help='IP address of interface to bind sockets to')
     parser.add_argument('--handler_directory', '-hd', dest='handler_dir', default='handlers/', help='path to folder containing files that define port handlers. See documentation for how to format handlers.')
     parser.add_argument('--log_directory', '-ld', dest='log_dir', default='logs/', help='path to directory to output log files to. Each service port will have its own logfile.')
     parser.add_argument('-v', action='store_true', default=False, help='enable verbose output')
