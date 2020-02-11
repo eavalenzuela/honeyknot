6a87db9e762e080d9e740e46cf4574d24ea54074
Eric Valenzuela <evalenzuela@thousandeyes.com>
2020-02-11 11:59:00 -0800

    config_file based socket ports, arg for threadcount

--- honeyknot.py
+++ honeyknot.py
@@ -64,20 +64,29 @@ def server_port_process(port_config_file, args):
     return port_config_file
 
 def server_port_thread_pool(port_config, args):
+    # Get port number from port_config file
+    cfg = configparser.ConfigParser()
+    try:
+        cfg.read(args.handler_dir+str(port_config))
+        socket_port = cfg['main']['port']
+    except Exception as e:
+        print('Error retrieving port number from config file: '+str(port_config))
+        print(e)
+        sys.exit(1)
     # Create thread pool
-    with ThreadPoolExecutor(5) as spte:
+    with ThreadPoolExecutor(int(args.thread_count)) as spte:
         # Create socket
-        print('standing-up socket on '+str(port_config)+'...')
+        print('standing-up socket on '+str(socket_port)+'...')
         try:
             conn_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             conn_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            conn_sock.bind((args.bind_ip, int(port_config)))
+            conn_sock.bind((args.bind_ip, int(socket_port)))
         except Exception as se:
             print('socket construction error: ')
             print(se)
             sys.exit(1)
         futures = []
-        for i in range(5):
+        for i in range(int(args.thread_count)):
             if args.tv:
                 print('|- running port handler '+str(port_config)+', thread '+str(i))
             futures.append(spte.submit(server_port_thread, [port_config, i], None, args))
@@ -115,15 +124,15 @@ def server_port_thread_pool(port_config, args):
                     print('connection from '+str(client_address)+' on port '+str(port_config))
 
                     client_info = [client_address, client_sock]
-                    futures.append(spte.submit(server_port_thread, [port_config, thread_num], client_info, args))
+                    futures.append(spte.submit(server_port_thread, port_config, thread_num, client_info, args))
     return futures
 
-def server_port_thread(counters, client_info, args):
-    print('|-- port handler: '+str(counters[0])+', thread: '+str(counters[1]))
+def server_port_thread(port_config, thread_num, client_info, args):
+    print('|-- port handler: '+str(port_config)+', thread: '+str(thread_num))
     
     # If first run, return
     if client_info == None:
-        return counters[1]
+        return thread_num
     else:
         client_address = client_info[0]
         client_connection = client_info[1]
@@ -132,7 +141,7 @@ def server_port_thread(counters, client_info, args):
             print(data)
 
     # Log raw request data
-    logpath = args.log_dir+str(counters[0])+'.log'
+    logpath = args.log_dir+str(port_config)+'.log'
     try:
         with open(logpath, 'a') as rl:
             rl.write(str(client_address)+': '+str(data)+'\n')
@@ -141,20 +150,21 @@ def server_port_thread(counters, client_info, args):
 
     # Execute hk_handler
     try:
-        hk_handler.hk_handler(counters[0], data, client_connection, args)
+        hk_handler.hk_handler(port_config, data, client_connection, args)
     except Exception as tk:
         print(tk)
-    return counters[1]
+    return thread_num
 
 def run():
     # Argument Processing
     parser = argparse.ArgumentParser()
-    parser.add_argument('-ip', '--bind_ip', dest='bind_ip', help='IP address of interface to bind sockets to')
+    parser.add_argument('-i', '--bind_ip', dest='bind_ip', help='IP address of interface to bind sockets to')
     parser.add_argument('--handler_directory', '-hd', dest='handler_dir', default='handlers/', help='path to folder containing files that define port handlers. See documentation for how to format handlers.')
     parser.add_argument('--definitions_directory', '-dd', dest='definition_dir', default='definition_files', help='path to folder containing service definiton json files')
     parser.add_argument('--log_directory', '-ld', dest='log_dir', default='logs/', help='path to folder to output log files to. Each service port will have its own logfile.')
     parser.add_argument('-v', action='store_true', default=False, help='enable verbose output')
     parser.add_argument('-tv', action='store_true', default=False, help='enable thread verbosity')
+    parser.add_argument('-tc', '--thread_count', dest='thread_count', default=5, help='number of threads for each port process to manage (default 5)')
     args = parser.parse_args()
 
     # Call main loop-handler
